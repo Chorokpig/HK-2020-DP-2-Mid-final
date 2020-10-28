04bb544926ec0eeb2dab2d354ebcf98d693ff063
Chorokpig <48345574+Chorokpig@users.noreply.github.com>
2020-10-28 17:46:21 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    console.log("Jquery Load complete!")
+})
+
+$(document).ready(function () {
+    $('.coco').plaxify({"xRange":40,"yRange":40})
+    $('.background').plaxify({"xRange":20,"yRange":20})
+    $.plax.enable()
+})
